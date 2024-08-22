774fa2a27c570abedbd7d8fc813fec25b09b086c
Prakash2608 <prakashraj822@gmail.com>
2024-08-22 12:00:50 +0530

    first attempt to make response

--- main.py
+++ main.py
@@ -12,21 +12,25 @@ bedrock = boto3.client(service_name = "bedrock-runtime")
 
 payload = {
     
-    
+        # "prompt": "[INST]"+prompt+"[/INST]",
+        "prompt": prompt,
+        "max_gen_len":512,
+        "temperature": 0.4,
+        "top_p": 0.9
 }
 
 body = json.dumps(payload)
-model_id = "meta.llama3-70b-instruct-v1"
+model_id = "meta.llama3-70b-instruct-v1:0"
 
 response = bedrock.invoke_model(
     body = body,
-    model_id = model_id,
+    modelId = model_id,
     accept = "application/json",
-    content_type = "application/json"
+    contentType = "application/json"
 )
 
 
-response_body = json.load(response.get("body").read())
+response_body = json.loads(response.get("body").read())
 
 response_text = response_body['generation']
 print(response_text)
